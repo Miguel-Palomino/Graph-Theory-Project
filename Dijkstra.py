b67c8796b35caaa9ca75011be67dd36b97669210
Miguel-Palomino <miguel.palomino@wsu.edu>
2024-11-14 18:50:13 -0800

    Split Program into class file and algorithm file

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,44 +1,6 @@
-'''from WeightedGraph import *'''
+from WeightedGraph import *
 import heapq
 import math
-
-class WeightedGraph:
-    def __init__(self, num_nodes):
-        self.num_nodes = num_nodes
-        # Initialize a 2D matrix with zeros
-        self.adj_matrix = [[0] * num_nodes for _ in range(num_nodes)]
-
-    def add_edge(self, node1, node2, w):
-        # TODO: Add an undirected edge between node1 and node2
-        self.adj_matrix[node1][node2] = w
-        self.adj_matrix[node2][node1] = w
-        #pass #placeholder for empty function to get ignored
-
-    def remove_edge(self, node1, node2):
-        # Remove the edge between node1 and node2
-        self.adj_matrix[node1][node2] = 0
-        self.adj_matrix[node2][node1] = 0
-
-    def num_edges(self):
-        num_edges = 0
-        for i in range(self.num_nodes):
-            for j in range(i+1, self.num_nodes):
-                if self.adj_matrix[i][j] != 0:
-                    num_edges += 1
-        return num_edges
-
-    def print_graph(self):
-        # Print the adjacency matrix
-        for row in self.adj_matrix:
-            print(" ".join(map(str, row)))
-
-# if __name__ == "__main__":
-#     # use case
-#     test_graph = WeightedGraph(3)
-#     test_graph.add_edge(1,0, 2)
-#     test_graph.add_edge(1, 2, 3)
-#     test_graph.print_graph()
-#     print(test_graph.num_edges())
 
 def build_tree(T):
     """Builds a dictionary representing the tree from the list of parents."""
