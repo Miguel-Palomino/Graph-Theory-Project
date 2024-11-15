cb197693cfb088cf82bf28ebe5dd0eaa5eda3bd8
Miguel-Palomino <miguel.palomino@wsu.edu>
2024-11-14 18:39:34 -0800

    Switched Prim's Algorithm to Dijkstra's Algorithm

--- main.py
+++ main.py
@@ -68,17 +68,15 @@ def print_tree(T):
     else:
         print("No root found")
 
-def Prim(g): #change this to Dijkstra's Algorithm
-
-    root = 0  # assign vertex 0 as the root
-    Visited = [False] * g.num_nodes # visited set
-    T = [-1] * g.num_nodes # tree
-    pq = []  # Priority queue to store vertices that are being processed
-    c = [math.inf] * g.num_nodes # vertex cost
-    c[0] = 0 # vertex cost of the root is 0
+def Dijkstra(g): #change this to Dijkstra's Algorithm
+    root = 0  # assign vertex 0 as the root/source
+    Visited = [False] * g.num_nodes # visited set; no vertices have been visited yet
+    distance = [math.inf] * g.num_nodes # vertex cost, set all vertices cost to infinity
+    path = [-1] * g.num_nodes # tree
+    distance[root] = 0  # distance to root is 0
+    pq = [] #initilize empty priority queue
     heapq.heappush(pq, (0, root)) # insert the first item to priority queue
 
-    # TODO: implement the while loop
     # See here https://docs.python.org/3/library/heapq.html for more information about heapq.
     while pq:
         cost, u = heapq.heappop(pq) #heappop pops and returns smallest item from the heap; smallest element is always at the root
@@ -88,11 +86,13 @@ def Prim(g): #change this to Dijkstra's Algorithm
         Visited[u] = True #if vertex was not visited, now visit it and do algorithm
         #to check for neighbors check the row of the vertex and find a nonzero value
         for v, w in enumerate(g.adj_matrix[u]): #v will hold the vertex adjacent to u and w will hold the weight of the edge conecting those vertices
-            if not Visited[v] and w < c[v] and w != 0: #if v is not visited and w of vertex is less than the cost already signed to and it is a nonzero value
-                T[v] = u #update the parent of v
-                c[v] = w #update cost of v
-                heapq.heappush(pq, (w, v)) #push new value into priority queue (pq, (cost, vertex))
-    return T #return minimum-weight spanning tree
+            if not Visited[v] and w > 0: #if v is not visited and w of vertex is less than the cost already signed to and it is a nonzero value
+                newCost = cost + w
+                if newCost < distance[v]:
+                    distance[v] = newCost
+                    path[v] = u
+                    heapq.heappush(pq, (newCost, v)) #push new value into priority queue (pq, (cost, vertex))
+    return distance, path #return distance and shortest path to any vertex
 
 
 # test
@@ -104,4 +104,5 @@ test_graph.add_edge(1,2, 1)
 test_graph.add_edge(2,3, 3)
 test_graph.add_edge(2,4, 10)
 test_graph.add_edge(3,4, 5)
-print_tree(Prim(test_graph))
+distance, path = Dijkstra(test_graph)
+print_tree(path)
